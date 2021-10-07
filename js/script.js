dc11b94d50a09a83b34d165fe29eeec9764519c0
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-07 13:23:09 +0200

    completed exercise

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//chiedo all'utente il nome
+const promptNome = prompt("Qual'è il tuo nome?");
+//chiedo all'utente il cognome
+const promptCognome = prompt("Qual'è il tuo cognome?");
+//chiedo all'utente il suo colore preferito
+const promptColorePreferito = prompt("Qual'è il tuo colore preferito?");
+
+//Saluto l'utente con nome e cognome 
+alert (promptNome + promptCognome + promptColorePreferito + 21);
